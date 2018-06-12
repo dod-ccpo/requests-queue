c3537b197d0f07cd3c2d8a27f0b20054f363d88c
richard-dds <richard-ctr@friends.dds.mil>
2018-06-12 10:24:11 -0400

    Add port to base config

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from requests_queue.make_app import make_app, make_config
 
 config = make_config()
 app = make_app(config)
-port = int( config['default'].get('PORT',8888) )
-app.listen(port)
-print("Listening on http://localhost:%i" % port)
-tornado.ioloop.IOLoop.current().start()
+
+if __name__ == '__main__':
+    port = int(config['default']['port'])
+    app.listen(port)
+    print("Listening on http://localhost:%i" % port)
+    tornado.ioloop.IOLoop.current().start()
