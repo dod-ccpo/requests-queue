542f289a77fc1acc1d53919db4393dc118a5e692
richard-dds <richard-ctr@friends.dds.mil>
2018-06-19 12:52:01 -0400

    Allow GET /requests to return one or many

--- requests_queue/handlers/request.py
+++ requests_queue/handlers/request.py
@@ -32,10 +32,21 @@ class RequestHandler(RequestHandler):
     def initialize(self, db_session):
         self.db_session = db_session
 
-    def get(self):
-        requests = self.db_session.query(Request).all()
-        serialized_requests = RequestSerializer().dump(requests, many=True).data
-        self.write({'requests': serialized_requests})
+    def get(self, request_id=None):
+        if request_id:
+            request = self.db_session.query(Request).get(request_id)
+            if request is not None:
+                serialized_request = RequestSerializer().dump(request).data
+                self.write(serialized_request)
+            else:
+                self.send_error(404)
+        else:
+            creator_id = self.get_argument('creator_id', None)
+            requests = (self.db_session.query(Request)
+                                       .filter(Request.creator == creator_id)
+                                       .all())
+            serialized_requests = RequestSerializer().dump(requests, many=True).data
+            self.write({'requests': serialized_requests})
 
     def patch(self, request_id):
         """
