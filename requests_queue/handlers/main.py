c5fffd99dfd1531d226333f31aa81c464d9fc166
richard-dds <richard-ctr@friends.dds.mil>
2018-06-25 10:36:07 -0400

    Add Content-Type header to all responses

--- requests_queue/handlers/main.py
+++
@@ -1,7 +0,0 @@
-from tornado.web import RequestHandler
-
-
-class MainHandler(RequestHandler):
-
-    def get(self):
-        return self.write({'hello': 'world'})
--- requests_queue/make_app.py
+++ requests_queue/make_app.py
@@ -7,15 +7,12 @@ from requests_queue.redis import make_redis
 from requests_queue.database import make_db
 from requests_queue.handlers.requests import RequestsHandler
 from requests_queue.handlers.user_requests import UserRequestsHandler
-from requests_queue.handlers.main import MainHandler
 
 
 def make_app(config, deps):
     db_session = deps['db']
 
     app = tornado.web.Application([
-            url( r'/', MainHandler),
-
             url(r'/api/v1/requests', RequestsHandler, {'db_session': db_session}),
             url(r'/api/v1/requests/(.*)', RequestsHandler, {'db_session': db_session}),
 
