b9a8fa3733f4a1db85b04724c1385efe31b73b27
richard-dds <richard-ctr@friends.dds.mil>
2018-07-09 12:04:32 -0400

    Set correct content-type for root endpoint

--- requests_queue/handlers/root.py
+++ requests_queue/handlers/root.py
@@ -4,4 +4,5 @@ from requests_queue.handlers.base import BaseHandler
 class RootHandler(BaseHandler):
 
     def get(self):
+        self.set_header('Content-Type', 'text/plain')
         self.write("Hello from requests-queue")
