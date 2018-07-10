690f2e408db8e94607591bda02daa93c92801514
richard-dds <richard-ctr@friends.dds.mil>
2018-07-10 10:24:36 -0400

    Add helper text to ipython session

--- ipython_setup.py
+++ ipython_setup.py
@@ -3,3 +3,5 @@ from requests_queue.database import make_db
 from requests_queue.models import *
 
 db = make_db(make_config())
+
+print("\nWelcome to requests-queue. This shell has all models in scope, and a SQLAlchemy session called db.")
