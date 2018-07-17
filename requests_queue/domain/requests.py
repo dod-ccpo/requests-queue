3561f87eaa73b9c21d8eacff34e74afbb923da68
richard-dds <richard-ctr@friends.dds.mil>
2018-07-17 10:43:25 -0400

    Factor POST /requests/submit into domain layer

--- requests_queue/domain/requests.py
+++ requests_queue/domain/requests.py
@@ -1,4 +1,8 @@
-from requests_queue.models import Request
+import tornado.gen
+from sqlalchemy.orm.exc import NoResultFound
+
+from requests_queue.models import Request, StatusEvent
+from .exceptions import NotFoundError
 
 class Requests(object):
 
@@ -6,7 +10,16 @@ class Requests(object):
         self.db_session = db_session
 
     def get(self, request_id):
-        return self.db_session.query(Request).get(request_id)
+        try:
+            request = (
+                self.db_session.query(Request)
+                .filter_by(id=request_id)
+                .one()
+            )
+        except NoResultFound:
+            raise NotFoundError("request")
+
+        return request
 
     def get_many(self, creator_id=None):
         filters = []
@@ -18,3 +31,21 @@ class Requests(object):
                                    .order_by(Request.time_created.desc())
                                    .all())
         return requests
+
+    @tornado.gen.coroutine
+    def submit(self, request):
+        request.status_events.append(StatusEvent(new_status="submitted"))
+
+        if Requests.should_auto_approve(request):
+            request.status_events.append(StatusEvent(new_status="approved"))
+
+        self.db_session.add(request)
+        self.db_session.commit()
+
+    @classmethod
+    def should_auto_approve(cls, request):
+        all_request_sections = ["details_of_use", "information_about_you", "primary_poc"]
+        existing_request_sections = request.body.keys()
+        return request.status == "submitted" and all(
+            section in existing_request_sections for section in all_request_sections
+        )
