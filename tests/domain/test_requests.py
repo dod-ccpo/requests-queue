97c26c2d9bca8a8e95663c23b5b5f7b94adce9c3
richard-dds <richard-ctr@friends.dds.mil>
2018-07-19 14:59:59 -0400

    Clean up domain tests

--- tests/domain/test_requests.py
+++ tests/domain/test_requests.py
@@ -1,12 +1,12 @@
 import pytest
 from uuid import uuid4
 
-from requests_queue.domain.requests import Requests
 from requests_queue.domain.exceptions import NotFoundError
 
 from tests.factories import RequestFactory
 
-def test_can_get_request(requests: Requests, db):
+
+def test_can_get_request(requests, db):
     created_request = RequestFactory.create()
     db.add(created_request)
     db.commit()
@@ -15,6 +15,20 @@ def test_can_get_request(requests: Requests, db):
 
     assert request.id == created_request.id
 
-def test_nonexistent_request_raises(requests: Requests):
+
+def test_nonexistent_request_raises(requests):
     with pytest.raises(NotFoundError):
         requests.get(uuid4())
+
+
+@pytest.mark.gen_test
+def test_auto_approve_200k_or_less(requests, db):
+    created_request = RequestFactory.create()
+    db.add(created_request)
+    db.commit()
+
+    created_request.body = {"details_of_use": {"dollar_value": 200000}}
+
+    request = yield requests.submit(created_request)
+
+    assert request.status == 'approved'
