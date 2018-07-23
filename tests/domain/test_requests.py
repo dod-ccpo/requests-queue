c61b864f167a22b94f86c104d647edfe2c08ff66
richard-dds <richard-ctr@friends.dds.mil>
2018-07-23 10:08:14 -0400

    Factor out new_request fixture

--- tests/domain/test_requests.py
+++ tests/domain/test_requests.py
@@ -5,15 +5,19 @@ from requests_queue.domain.exceptions import NotFoundError
 
 from tests.factories import RequestFactory
 
-
-def test_can_get_request(requests, db):
+@pytest.fixture(scope="function")
+def new_request(db):
     created_request = RequestFactory.create()
     db.add(created_request)
     db.commit()
 
-    request = requests.get(created_request.id)
+    return created_request
 
-    assert request.id == created_request.id
+
+def test_can_get_request(requests, new_request):
+    request = requests.get(new_request.id)
+
+    assert request.id == new_request.id
 
 
 def test_nonexistent_request_raises(requests):
@@ -22,39 +26,24 @@ def test_nonexistent_request_raises(requests):
 
 
 @pytest.mark.gen_test
-def test_auto_approve_less_than_1m(requests, db):
-    created_request = RequestFactory.create()
-    db.add(created_request)
-    db.commit()
-
-    created_request.body = {"details_of_use": {"dollar_value": 999999}}
-
-    request = yield requests.submit(created_request)
+def test_auto_approve_less_than_1m(requests, new_request):
+    new_request.body = {"details_of_use": {"dollar_value": 999999}}
+    request = yield requests.submit(new_request)
 
     assert request.status == 'approved'
 
 
 @pytest.mark.gen_test
-def test_dont_auto_approve_if_dollar_value_is_1m_or_above(requests, db):
-    created_request = RequestFactory.create()
-    db.add(created_request)
-    db.commit()
-
-    created_request.body = {"details_of_use": {"dollar_value": 1000000}}
-
-    request = yield requests.submit(created_request)
+def test_dont_auto_approve_if_dollar_value_is_1m_or_above(requests, new_request):
+    new_request.body = {"details_of_use": {"dollar_value": 1000000}}
+    request = yield requests.submit(new_request)
 
     assert request.status == 'submitted'
 
 
 @pytest.mark.gen_test
-def test_dont_auto_approve_if_no_dollar_value_specified(requests, db):
-    created_request = RequestFactory.create()
-    db.add(created_request)
-    db.commit()
-
-    created_request.body = {"details_of_use": {}}
-
-    request = yield requests.submit(created_request)
+def test_dont_auto_approve_if_no_dollar_value_specified(requests, new_request):
+    new_request.body = {"details_of_use": {}}
+    request = yield requests.submit(new_request)
 
     assert request.status == 'submitted'
